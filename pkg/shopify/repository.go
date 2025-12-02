4207105db7db03cf9ff139ddd4f944f8c961a37d
Martin Valle <mdvh05@gmail.com>
2025-12-02 15:35:00 -0400

    feat: Add user document data retrieval from Shopify metafields and persistence to user profiles.

--- pkg/shopify/repository.go
+++ pkg/shopify/repository.go
@@ -11,8 +11,9 @@ import (
 )
 
 const (
-	customNamespace = "custom"
-	orderDogDataKey = "dog_data"
+	customNamespace  = "custom"
+	orderDogDataKey  = "dog_data"
+	orderUserDataKey = "user_data"
 )
 
 // Repository defines methods to interact with Shopify API
@@ -20,6 +21,7 @@ type Repository interface {
 	GetDogData(ctx context.Context, gid string) (*Pets, error)
 	GetVariantByID(ctx context.Context, gid string) (*Variant, error)
 	CreateOrder(ctx context.Context, input any) (*OrderCreateResponse, error)
+	GetUserData(ctx context.Context, gid string) (*User, error)
 }
 
 // Repository is a Shopify API repository
@@ -52,7 +54,7 @@ func (r *repository) GetDogData(
 	}
 	intent := 0
 	var resp GetOrderMetafieldResponse
-	for intent < 5 {
+	for intent < 10 {
 		if err := r.gql.Do(ctx, getOrderMetafield, vars, &resp); err != nil {
 			r.Logger.Error("failed to get order metafield", zap.Error(err), zap.Any("vars", vars))
 			return nil, err
@@ -77,6 +79,47 @@ func (r *repository) GetDogData(
 	}
 
 	return &pets, nil
+}
+
+// GetUserData retrieves user data from the Shopify API.
+func (r *repository) GetUserData(
+	ctx context.Context, gid string,
+) (*User, error) {
+	if !strings.Contains(gid, orderKind) {
+		gid = GID(orderKind, gid)
+	}
+	vars := map[string]any{
+		"id":        gid,
+		"namespace": customNamespace,
+		"key":       orderUserDataKey,
+	}
+	intent := 0
+	var resp GetOrderMetafieldResponse
+	for intent < 10 {
+		if err := r.gql.Do(ctx, getOrderMetafield, vars, &resp); err != nil {
+			r.Logger.Error("failed to get order metafield", zap.Error(err), zap.Any("vars", vars))
+			return nil, err
+		}
+
+		if resp.Order.Metafield != nil {
+			break
+		}
+		intent++
+		time.Sleep(1 * time.Second)
+	}
+
+	if resp.Order.Metafield == nil {
+		r.Logger.Error("order dog data metafield not found", zap.Any("vars", vars))
+		return nil, errors.New("order dog data metafield not found")
+	}
+
+	var user User
+	if err := json.Unmarshal(resp.Order.Metafield.JsonValue, &user); err != nil {
+		r.Logger.Error("failed to unmarshal order dog data", zap.Error(err), zap.Any("jsonValue", string(resp.Order.Metafield.JsonValue)))
+		return nil, err
+	}
+
+	return &user, nil
 }
 
 // GetVariantByID retrieves a product variant by its ID.
